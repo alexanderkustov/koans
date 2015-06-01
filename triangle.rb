e2dd4539cb45cd2b5d623230a9c09ff0ad63f5b4
Alexander K <knuxus@gmail.com>
2015-06-01 10:01:09 +0100

    expections

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+ 
+  if (a==b) && (b==c)
+  	:equilateral
+  else
+  	if (a==b) or (b==c) or (c==a)
+  		:isosceles
+  	else
+  		:scalene
+  	end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
